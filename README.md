153638823b4be3a62d0ef116ec7cc3ccebc710f3
AfonsoCunha22 <64204008+AfonsoCunha22@users.noreply.github.com>
2025-01-29 16:18:22 +0100

    File and Folder structure overhaul. Removed the previous files to restart the project, now dowloading kaggle files and putting them into data/raw

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MusicSetAnalysis
+
+All the code files are located on the **src** folder. Where it contins the following files:
+- data_process.py: This file is responsible for loading the data via the kagglehub library and preprocess the data
+- model_training.py: Model creating and training
+- Neo_Classical_Analysis.ipynb: This file contains the comparison and analysis of new and more recent composers on a model trained on more classical composers.
---
+++ src/data_process.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+import kagglehub
+
+# Processed data
+def download_data():
+    try:
+        # Download latest version
+        path = kagglehub.dataset_download("imsparsh/musicnet-dataset")
+        print("Path to dataset files:", path)
+        return path
+    except Exception as e:
+        print(f"Error downloading or loading data: {e}")
+        return None
+
+
+def move_data(source_directory, target_directory):
+    try:
+        if not os.path.exists(target_directory):
+            os.makedirs(target_directory)
+        for file in os.listdir(source_directory):
+            source_file = os.path.join(source_directory, file)
+            target_file = os.path.join(target_directory, file)
+            if os.path.isfile(source_file):
+                shutil.move(source_file, target_file)
+            else:
+                shutil.move(source_file, target_directory)
+
+    except Exception as e:
+        print(f"Error moving data: {e}")
+
+def main():
+    dataset_path = download_data()
+    if dataset_path:
+        target_directory = "data/raw"
+        move_data(dataset_path, target_directory)
+
+        
+if __name__ == "__main__":
+    main()
